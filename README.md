2dfc341ae1c6465518e49a444e45dee6bd70a9a3
Chris Komaroff <ckom9@example.com>
2018-02-17 17:01:30 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Django project for testing Bootstrap 4
+
+bstest is a Django project for testing Bootstrap 4 (or whichever version of
+bootstrap you install). *I created this from Treehouse course in Bootstrap 4 in Fall 2017. Lots of good boostrap examples here.*
+
+### Instructions for unix/aix command line:
+
+1. clone this repo ... see GitHub docs ... git clone <URL for this repo>
+2. cd .../bstest *(i.e. cd to bstest directory, wherever you put it).*
+3. python3 manage.py runserver *(or python3.5 manage.py runserver for some systems)*
+4. browse to 127.0.0.1:8000
+5. note cool bootstrap being used ...
+
+2/17/18 cdk
---
+++ bstest/urls.py
@@ -0,0 +1,37 @@
+"""bstest URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+
+from . import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r"^$", views.Home.as_view(), name="home"),
+]
+
+
+# Django Debug Toolbar - update urlpatterns to allow display.
+# This was not necessary with DDTB 1.7 (before 1.8). 10/18/17
+# https://django-debug-toolbar.readthedocs.io/en/stable/installation.html
+from django.conf import settings
+from django.conf.urls import include, url
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
